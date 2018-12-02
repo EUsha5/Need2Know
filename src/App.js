0d5ff0346e1969504f251b2170778e4caefd71f5
Herman White <phoenixquint@gmail.com>
2018-12-01 20:42:20 -0500

    One last broken commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Auth0WebClient } from "@8base/auth";
 import { EightBaseAppProvider } from "@8base/app-provider";
 import { EightBaseBoostProvider, AsyncContent } from "@8base/boost";
-import { ToastContainer, toast } from "react-toastify";
 import Home from "./components/Home";
 import Admin from "./components/Admin";
 import User from "./components/User";
